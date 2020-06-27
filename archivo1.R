f2ef5781faae28b78996bf84278d830a23679534
Danilo Urias <d14urias@gmail.com>
2020-06-27 13:24:03 -0600

    first commit

---
+++ archivo1.R
@@ -0,0 +1,46 @@
+
+#Grafico 1 - Total de ganancias en Paises de Centro America
+
+df_sales <- read.csv(file = 'C:/Archivos_P2/sales.csv')
+sub_tabla <- df_sales[c("Country","Item.Type","Total.Profit")]
+sub_tabla <-subset.data.frame(sub_tabla,sub_tabla$Item.Type=="Clothes")
+sub_tabla <- subset.data.frame(sub_tabla,sub_tabla$Country=="Guatemala" |sub_tabla$Country=="Belize"|sub_tabla$Country=="El Salvador" | sub_tabla$Country=="Honduras" |sub_tabla$Country=="Nicaragua" |sub_tabla$Country=="Costa Rica" |sub_tabla$Country=="Panama" |sub_tabla$Country=="Belice")
+sub_tabla <- aggregate(x=sub_tabla$Total.Profit, by=list(Paises_CA=sub_tabla$Country), FUN=sum)
+
+sub_tabla
+
+pie(sub_tabla$x,labels = sub_tabla$x,clockwise = TRUE,col = rainbow(length(sub_tabla$x)),cex=0.8, main =  "TOTAL DE VENTAS DE ROPA EN PAISES DE CENTRO AMERICA")
+legend("left",legend=sub_tabla$Paises_CA,bty="n",fill=rainbow(length(sub_tabla$x)),cex = 0.8)
+
+
+
+#Grafico 2 - Articulos vendidos offline y online
+
+df_sales <- read.csv(file = 'C:/Archivos_P2/sales.csv')
+sub_tabla <- df_sales[c("Units.Sold","Sales.Channel")]
+sub_tabla <- aggregate(x=sub_tabla$Units.Sold, by=list(canales=sub_tabla$Sales.Channel), FUN=sum)
+
+sub_tabla
+
+barplot(sub_tabla$x, main="Numero de Articulos de las ventas Offline vs Online",
+        xlab="Canales de distribucion", col=c("darkblue","red"),
+        names.arg= sub_tabla$x,xlim = c (0, 3))
+
+legend("right",legend=sub_tabla$canales,bty="n",fill=c("darkblue","red"),cex = 0.8)
+
+
+
+
+#Grafico 3 - Año con mas ordenes de prioridad M 
+
+df_sales <- read.csv(file = 'C:/Archivos_P2/sales.csv')
+sub_tabla <- df_sales[c("Order.Priority","Order.Date")]
+sub_tabla <-subset.data.frame(sub_tabla,sub_tabla$Order.Priority=="M")
+sub_tabla <- cbind(sub_tabla,anio= format(as.Date(sub_tabla$Order.Date, "%m/%d/%Y"),"%Y"),contador=c(1))
+sub_tabla <- aggregate(x=sub_tabla$contador, by=list(anios=sub_tabla$anio), FUN=sum)
+sub_tabla <-sub_tabla[order(sub_tabla$x, decreasing = TRUE),]
+
+mybar<-barplot(sub_tabla$x, main="Años con mas ordenes de prioridad M",
+        xlab="Años",ylab="Ordenes", col=rainbow(length(sub_tabla$x)),
+        names.arg= paste(sub_tabla$anios,sub_tabla$x, sep="\n"),xlim = c (0, 15),ylim = c (0, 200),legend = sub_tabla$anios)
+
